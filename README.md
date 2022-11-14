2aa08dc8279b295e4070b37059065e39be8f6684
Sophia Mersmann <sophia.mersmann1@gmail.com>
2022-11-14 09:38:39 +0100

    move to cloud function (#1)
    * move data to data folder
    
    * write update as cloud function
    
    * remove temporary file

--- README.md
+++ README.md
@@ -1,11 +1,41 @@
 # Update consumer price index database
 
-The script `update-database.py` updates the [consumer price index database](https://console.cloud.google.com/sql/instances/consumer-price-index/overview?project=rbb-data-inflation) when new data is available from [Genesis](https://www-genesis.destatis.de/genesis//online?operation=table&code=61111-0006&bypass=true&levelindex=0&levelid=1657617156882#abreadcrumb). It uses the database's API at https://europe-west3-rbb-data-inflation.cloudfunctions.net/consumer-price-index-api ([GitHub](https://github.com/rbb-data/consumer-price-index-api))
+The function `update_database` in `main.py` updates the [consumer price index database](https://console.cloud.google.com/sql/instances/consumer-price-index/overview?project=rbb-data-inflation) when new data is available from [Genesis](https://www-genesis.destatis.de/genesis//online?operation=table&code=61111-0006&bypass=true&levelindex=0&levelid=1657617156882#abreadcrumb). It uses the database's API at https://europe-west3-rbb-data-inflation.cloudfunctions.net/consumer-price-index-api ([GitHub](https://github.com/rbb-data/consumer-price-index-api))
 
-Secrets are not stored in version control. To run this script, add `secret_config.py` and add the following lines:
+## Development
 
-```python
-GENESIS_USERNAME = ""
-GENESIS_PASSWORD = ""
-API_TOKEN = ""
+### Run locally
+
+> **Note**
+>
+> Add the necessary credentials by downloading the key file that belongs to the service account `rbb-data-inflation@appspot.gserviceaccount.com` and store the downloaded file as `rbb-data-inflation-fc4113adea34.json`.
+
+Load environment variables from `.env`:
+
+```bash
+export $(cat .env | xargs)
+```
+
+Install dependencies from `requirement.txt` and run the function:
+
+```bash
+python dev.py
+```
+
+### Deploy
+
+Set cloud project
+
+```bash
+gcloud config set project rbb-data-inflation
+```
+
+and run:
+
+```
+gcloud functions deploy update-consumer-price-index-database \
+  --region=europe-west3 \
+  --runtime=python310 \
+  --entry-point=update_database \
+  --trigger-topic=database-update
 ```
--- main.py
+++ main.py
@@ -1,32 +1,31 @@
-#!/usr/bin/env python3
-
-import datetime
-import locale
-import logging
 import os
-import sys
+import datetime
+import logging
 
 import numpy as np
 import pandas as pd
 import requests
 from dateutil.relativedelta import relativedelta
 
-import secret_config as CONFIG
+from google.cloud import secretmanager
 
-locale.setlocale(locale.LC_TIME, "de_DE.UTF-8")
+client = secretmanager.SecretManagerServiceClient()
+
 logging.basicConfig(
     format="%(asctime)s [%(levelname)s] %(message)s",
     datefmt="%d-%m-%y %H:%M:%S",
     level=logging.INFO,
 )
 
-IDS_FILENAME = "warenkorb_ids.txt"
+root = os.path.dirname(os.path.abspath(__file__))
+IDS_FILENAME = os.path.join(root, "data", "warenkorb_ids.txt")
 
-SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
-IDS_PATH = os.path.join(SCRIPT_DIR, IDS_FILENAME)
+if "FUNCTION_ENV" in os.environ and os.environ["FUNCTION_ENV"] == "development":
+    GENESIS_FILENAME = os.path.join(root, "tmp", "genesis_tmp.csv")
+else:
+    GENESIS_FILENAME = os.path.join("/tmp/genesis_tmp.csv")
 
-GENESIS_API_URL = "https://www-genesis.destatis.de/genesisWS/rest/2020/data/tablefile"
-API_URL = "https://europe-west3-rbb-data-inflation.cloudfunctions.net/consumer-price-index-api"
+os.makedirs(os.path.dirname(GENESIS_FILENAME), exist_ok=True)
 
 
 def download(url, params, filename):
@@ -72,9 +71,21 @@ def parse_raw_data(filename):
     df = df[COLUMNS.keys()].rename(columns=COLUMNS)
 
     # transform months into numerical format, e.g. Februar -> 2
-    df["month"] = df.month.apply(
-        lambda month: datetime.datetime.strptime(month, "%B").date().month
-    )
+    MONTH_NUMBER = {
+        "Januar": 1,
+        "Februar": 2,
+        "März": 3,
+        "April": 4,
+        "Mai": 5,
+        "Juni": 6,
+        "Juli": 7,
+        "August": 8,
+        "September": 9,
+        "Oktober": 10,
+        "November": 11,
+        "Dezember": 12,
+    }
+    df["month"] = df.month.apply(lambda month: MONTH_NUMBER[month])
 
     # drop rows without data
     df = df[df.value != "..."]
@@ -89,14 +100,20 @@ def parse_raw_data(filename):
     return df
 
 
-def main():
+def access_secret(name):
+    response = client.access_secret_version(request={"name": name})
+    return response.payload.data.decode("UTF-8")
+
+
+def update_database(event, context):
+    """Triggered from a message on a Cloud Pub/Sub topic."""
     # read warenkorb items to be requested from file
-    with open(IDS_PATH) as f:
+    with open(IDS_FILENAME) as f:
         item_ids = [_id.strip() for _id in f.readlines()]
 
     try:
         response = requests.get(
-            API_URL,
+            os.environ["API_URL"],
             params={
                 "table": "consumer-price-index",
                 "mode": "most-recent-date",
@@ -105,19 +122,19 @@ def main():
         )
     except Exception as e:
         logging.error("GET request failed: " + str(e))
-        sys.exit(1)
+        return
 
     if not response.ok:
         logging.error("GET request failed: " + str(response.url))
-        sys.exit(1)
+        return
 
     most_recent_date = response.json()
     curr_month = datetime.date(most_recent_date["year"], most_recent_date["month"], 1)
     next_month = curr_month + relativedelta(months=+1)
 
     genesis_query_params = {
-        "username": CONFIG.GENESIS_USERNAME,
-        "password": CONFIG.GENESIS_PASSWORD,
+        "username": os.environ["GENESIS_USERNAME"],
+        "password": access_secret(os.environ["GENESIS_PASSWORD"]),
         "language": "de",
         "name": "61111-0006",
         "area": "all",
@@ -129,36 +146,37 @@ def main():
     }
 
     # download data from genesis as csv file
-    filename = os.path.join(SCRIPT_DIR, "tmp.csv")
     download_response = download(
-        GENESIS_API_URL, params=genesis_query_params, filename=filename
+        os.environ["GENESIS_API_URL"],
+        params=genesis_query_params,
+        filename=GENESIS_FILENAME,
     )
     if not download_response["ok"]:
         logging.error("GET request failed: " + str(download_response["message"]))
-        sys.exit(1)
+        return
 
     # parse csv file and get updated data
-    df = parse_raw_data(filename)
+    df = parse_raw_data(GENESIS_FILENAME)
     df_updated = df[df.month >= next_month.month]
+
+    # clean up
+    if os.path.exists(GENESIS_FILENAME):
+        os.remove(GENESIS_FILENAME)
 
     if len(df_updated) == 0:
         logging.info("No new data available")
-        sys.exit(0)
+        return
 
     updated_records = df_updated.replace({np.nan: None}).to_dict("records")
 
-    logging.info(f"POST {API_URL} ({len(updated_records)} items)")
+    logging.info(f"POST {os.environ['API_URL']} ({len(updated_records)} items)")
     post_response = requests.post(
-        API_URL,
+        os.environ["API_URL"],
         params={"table": "consumer-price-index"},
         json=updated_records,
-        headers={"Authorization": "Bearer " + CONFIG.API_TOKEN},
+        headers={"Authorization": "Bearer " + access_secret(os.environ["API_SECRET"])},
     )
 
     if not post_response.ok:
         logging.error(f"POST request failed: {post_response.status_code}")
-        sys.exit(1)
-
-
-if __name__ == "__main__":
-    main()
+        return
